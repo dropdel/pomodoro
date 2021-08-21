624695e1e4224cbc300d4e003faf726a366f3183
dropdel <derrickethornton@protonmail.com>
2021-08-21 01:14:27 -0700

    Depending on the menuitem chosen, this affects the displayed value in the time element

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 const pomo = document.querySelector(".pomodoro");
 const lbreak = document.querySelector(".lbreak");
 const sbreak = document.querySelector(".sbreak");
-
+const timer = document.querySelector("time");
 function removeStyling(el) {
   el.style.background = "rgb(27, 21, 68)";
   el.style.opacity = "0.5";
@@ -15,4 +15,11 @@ function bgHighlight(e) {
   e.target.style.background = "red";
   e.target.style.opacity = "1";
   e.target.style.fontWeight = "bolder";
+  if (e.target.innerHTML.includes("pomodoro")) {
+    timer.innerHTML = "25:00";
+  } else if (e.target.innerHTML.includes("short break")) {
+    timer.innerHTML = "5:00";
+  } else {
+    timer.innerHTML = "20:00";
+  }
 }
