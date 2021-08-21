a5a81ca965487a4d514bc8eff621b5ac27d8a88d
dropdel <derrickethornton@protonmail.com>
2021-08-21 01:05:06 -0700

    Added a mousedown event on every relevant element: li tag. This click event should work on mobile now.

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,16 @@ const pomo = document.querySelector(".pomodoro");
 const lbreak = document.querySelector(".lbreak");
 const sbreak = document.querySelector(".sbreak");
 
-document.querySelectorAll("li").forEach((item) => {
-  item.addEventListener("click", (e) => {
-    removeStyling(pomo);
-    removeStyling(lbreak);
-    removeStyling(sbreak);
-    e.path["0"].style.background = "red";
-    e.path["0"].style.opacity = "1";
-    e.path["0"].style.fontWeight = "bolder";
-  });
-});
-
 function removeStyling(el) {
   el.style.background = "rgb(27, 21, 68)";
   el.style.opacity = "0.5";
   el.style.fontWeight = "lighter";
+}
+function bgHighlight(e) {
+  removeStyling(pomo);
+  removeStyling(lbreak);
+  removeStyling(sbreak);
+  e.target.style.background = "red";
+  e.target.style.opacity = "1";
+  e.target.style.fontWeight = "bolder";
 }
